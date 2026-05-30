da861806e695a3ea175e3f5edfb476bb08fd7060
Guga <luiz.antunesleite@gmail.com>
2026-05-30 20:50:46 -0300

    Gustavo is over 1.

--- src/main/java/game/creature/Pokemon.java
+++ src/main/java/game/creature/Pokemon.java
@@ -1,7 +1,71 @@
 package game.creature;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import game.creature.move.Move;
+import game.creature.move.StatType;
+import game.creature.move.status.StatusEffect;
+import javax.naming.Context;
+
 public class Pokemon {
     private Integer currentAccuracy;
+    private Integer id;
+    private String nickname;
+    private Integer currentHp;
+    private Species species;
+    private Stats currentStats;
+    private Integer currentLevel;
+    private Integer currentExperience;
+    private final Move[] moves = new Move[4];
+    private final List<StatusEffect> statusEffects = new ArrayList<>();
+
+
+
+    public Boolean isAlive(){
+        return currentHp>0;
+    }
+
+    public Integer receiveDamage(Integer damage){
+        currentHp-=damage;
+        return currentHp;
+    }
+    public Integer heal(Integer heal){
+        if(currentHp+heal<=currentStats.getValue(StatType.HP)){
+            currentHp+=heal;
+        }else{
+            currentHp=currentStats.getValue(StatType.HP);
+        }
+        return currentHp;
+    }
+
+    /*public Boolean applyStatus(StatusEffect statusEffect, BattleContext context){
+        
+    }*/
+
+    /*public Stats getEffectiveStats(BattleContext context){
+    
+    }*/
+
+    /*public Integer getEffectiveStat(StatType statType, BattleContext context){
+    
+    }*/
+
+    /*public void resolveStatusAtTurnStart(BattleContext context){
+    
+    }*/
+
+    /*public void resolveStatusAtTurnEnd(BattleContext context){
+    
+    }*/
+
+    
+
+    public Boolean replaceMove(Integer slot, Move currentMove){
+        Move oldMove = moves[slot];
+        moves[slot]=currentMove;
+        return oldMove==null;
+    }
 
     public Integer getCurrentAccuracy() {
         return currentAccuracy;
@@ -9,5 +73,16 @@ public class Pokemon {
 
     public void setCurrentAccuracy(Integer currentAccuracy) {
         this.currentAccuracy = currentAccuracy;
+    }
+    
+    public void levelUp(){
+        currentLevel++;
+        currentStats = currentStats.scaleForLevel(currentLevel);
+        currentHp = currentStats.getValue(StatType.HP);
+        for(var mv:moves){
+            if(mv!=null){
+                mv.restorePP();
+            }
+        }
     }
 }
