f301e4c8b9335fe331096f07d922ad09d8f997e2
PedroGrivicich <pedro.aguiar@aln.senaicimatec.edu.br>
2026-06-09 08:52:43 -0300

    a

---
+++ src/main/java/game/ui/shop/ShopHud.java
@@ -0,0 +1,14 @@
+package game.ui.shop;
+
+import java.awt.Color;
+
+import engine.ui.elements.UiImage;
+
+public class ShopHud extends UiImage{
+
+    public ShopHud(int sizeX, int sizeY, Color color) {
+        super(sizeX, sizeY, color);
+        //TODO; unimplemented method
+    }
+    
+}
